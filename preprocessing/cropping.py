53085c760ff75aa2840a6c11e883de06e13b825b
Sarah <sarah.flohr@hsbi.de>
2023-10-26 13:46:55 +0200

    add robust crop for image preprocessing

--- preprocessing/cropping.py
+++ preprocessing/cropping.py
@@ -1,22 +1,26 @@
-# image size 200x200
 import cv2
 import os
 from os import listdir
+from tqdm import tqdm
+from resizeImage import resizeAndPad
 
 # get the path/directory
 path_out = "C:/Users/sarah/OneDrive/Desktop/Deep Learning Info&Daten/test"
 path_in = "C:/Users/sarah/Deep-Learning/data/monkey"
 i = 0
 
-for images in os.listdir(path_in):
+for images in tqdm(os.listdir(path_in)):
     # read image from folder
     input_image = os.path.join(path_in, images)
     img = cv2.imread(input_image)
-    i = i + 1
+
     # crop image
-    crop_img = img[0:200, 0:200]
-    filename = "{}.jpg".format(i)
+    cropped_img = resizeAndPad(img, (200, 200))
+
+    # name new image
+    i = i + 1
+    filename = "monkey_{}.jpg".format(i)
 
     # save image to new folder
-    cv2.imwrite(os.path.join(path_out, filename), crop_img)
+    cv2.imwrite(os.path.join(path_out, filename), cropped_img)
     cv2.waitKey(0)
