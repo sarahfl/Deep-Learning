6b17fcf4de121cf47008b210080d081f4259b629
Sarah <sarah.flohr@hsbi.de>
2023-10-29 10:47:44 +0100

    add ml pipeline

--- model/mlPipeline.py
+++ model/mlPipeline.py
@@ -1,3 +1,13 @@
+import models
+
+batch = 32
 epochs = 50
 
-# for epoch in range(epochs):
+for batch in range(batch):
+    for epoch in range(epochs):
+        train, val = models.getTestDataset(
+            "Train_Test_Folder/test", batch_size=batch, size_img=(200, 200)
+        )
+        model = models.loadModelTraining("mobileNetV2Scratch", 200, dropout=0.2)
+        history = models.trainModel(model, train, val, epoch, "model")
+        plot = models.plotModel(history, "testPlot")
