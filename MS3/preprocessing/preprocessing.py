d9e0030049c9de4bec4a77c94b89c606f77657be
Sarah Flohr <sarah.flohr@hsbi.de>
2023-11-30 14:04:00 +0100

    update plots

--- MS3/preprocessing/preprocessing.py
+++ MS3/preprocessing/preprocessing.py
@@ -28,113 +28,64 @@ def renameImages():
         cv2.imwrite(os.path.join(path_out, filename), img)
         cv2.waitKey(0)
 
-
-def classDistribution():
-    df = pd.DataFrame(columns=['train_folder', 'count'])
-    parent_folder = "/home/sarah/Deep-Learning/MS3/preprocessing/Train_Test_Folder/train/"
-    for folder in tqdm(os.listdir(parent_folder)):
-        folder_path = os.path.join(parent_folder, folder)
-
-        image_count = sum(1 for image in os.listdir(folder_path))
-
-        print(image_count)
-        df = df._append({'train_folder': folder, 'count': image_count}, ignore_index=True)
-
-    print(df)
-    # df.to_csv('val_classDistribution.csv', sep='\t')
-
-    # Bar-Plot erstellen
-
-    plt.bar(df['train_folder'], df['count'])
-    plt.title('Verteilung der Bilder über die 10 Klassen')
-    # Achsentitel hinzufügen
-    plt.xlabel('Klasse')
-    plt.ylabel('Anzahl der Bilder')
-
-    # Diagramm anzeigen
-
-    plt.savefig('train_classDistribution.png')
-    plt.show()
-
-
-def copyImageas():
-    # get the path/directory
-    path_in = "/home/sarah/Deep-Learning/MS3/preprocessing/MS3_rawData/noFace"
-    path_out = "/home/sarah/Deep-Learning/MS3/preprocessing/Data"
-
-    for image in tqdm(os.listdir(path_in)):
-        input_image = os.path.join(path_in, image)
-        # read image
-        img = cv2.imread(input_image)
-
-        # save image to new folder
-        cv2.imwrite(os.path.join(path_out, image), img)
-        cv2.waitKey(0)
-
-
 def ageDistribution():
-    path_in = "/home/sarah/Desktop/UTKFace_2"
+    df_face = pd.read_csv('/home/sarah/Deep-Learning/MS3/preprocessing/Data/Face_classification.csv', index_col=0)
+    df_noFace = pd.read_csv('/home/sarah/Deep-Learning/MS3/preprocessing/Data/noFace_classification.csv', index_col=0)
+    df = pd.concat([df_face, df_noFace], axis=0, ignore_index=True)
 
-    arr = []
-    for image in tqdm(os.listdir(path_in)):
-        # split image name
-        image_split = image.split("_")
+    df = df['age'].to_list()
+    age0 = df.count('age0')
+    age1 = df.count('age1')
+    age2 = df.count('age2')
+    age3 = df.count('age3')
+    age4 = df.count('age4')
+    age5 = df.count('age5')
+    age6 = df.count('age6')
+    age7 = df.count('age7')
 
-        arr.append(int(image_split[0]))
 
-    # count occurrence of age
-    sorted_arr = np.sort(arr)
-    result = Counter(sorted_arr)
-    dict(result)
-    dict(sorted(result.items()))
-
-    age = result.keys()
-    counts = result.values()
+    age = ['1-2', '3-9', '10-20', '21-27', '28-45', '46-65', '66-116', 'no age']
+    counts = [age0, age1, age2, age3, age4, age5, age6, age7]
     fontsize = 20
 
     plt.figure(figsize=(40, 18))
-    plt.bar(age, counts)
+    bar_colors = ['tab:blue', 'tab:blue', 'tab:blue', 'tab:blue', 'tab:blue', 'tab:blue', 'tab:blue', 'tab:orange']
+    plt.bar(age, counts, color = bar_colors)
     plt.title("Distribution of Age in UTKFace", fontsize=fontsize)
     plt.xlabel("Age", fontsize=fontsize)
     plt.ylabel("Number of Images", fontsize=fontsize)
-    dim = np.arange(1, 117, 1)
-    plt.xticks(dim, fontsize=fontsize, rotation=90)
+
+    plt.xticks( fontsize=fontsize)
     plt.yticks(fontsize=fontsize)
 
-    plt.savefig('age_distribution_utkface.png')
+    plt.savefig('plots/age_distribution_classification.png')
     plt.show()
 
-
 def genderDistribution():
-    path_in = "/home/sarah/Desktop/UTKFace_2"
+    df_face = pd.read_csv('/home/sarah/Deep-Learning/MS3/preprocessing/Data/Face_classification.csv', index_col=0)
+    df_noFace = pd.read_csv('/home/sarah/Deep-Learning/MS3/preprocessing/Data/noFace_classification.csv', index_col=0)
+    df = pd.concat([df_face, df_noFace], axis=0, ignore_index=True)
 
-    arr = []
-    for image in tqdm(os.listdir(path_in)):
-        # split image name
-        image_split = image.split("_")
+    df = df['gender'].to_list()
+    male = df.count(0)
+    female = df.count(1)
+    no_gender = df.count(3)
 
-        arr.append(int(image_split[1]))
+    gender = ['male', 'female', 'no gender']
+    counts = [male, female, no_gender]
+    fontsize = 20
 
-    # count occurrence of age
-    sorted_arr = np.sort(arr)
-    result = Counter(sorted_arr)
-    dict(result)
-    dict(sorted(result.items()))
-
-    gender = ['male', 'female']
-    counts = result.values()
+    plt.figure(figsize=(20, 20))
     fig, ax = plt.subplots()
-    bar_colors = ['tab:blue', 'tab:red']
+    bar_colors = ['tab:blue', 'tab:blue', 'tab:orange']
     ax.bar(gender, counts, color=bar_colors)
 
     ax.set_ylabel('Number of Images')
     ax.set_title('Distribution of gender')
 
-    plt.savefig('gender_distribution_utkface.png')
+    plt.savefig('plots/gender_distribution.png')
 
     plt.show()
-
-
 
 def createFaceCSV_classification():
     # get the path/directory
@@ -170,7 +121,6 @@ def createFaceCSV_classification():
 
     df.to_csv('Data/Face_classification.csv')
 
-
 def createNoFaceCSV_classification():
     # get the path/directory
     path_in = "/home/sarah/Deep-Learning/MS3/preprocessing/MS3_rawData/noFace"
@@ -182,7 +132,6 @@ def createNoFaceCSV_classification():
         df = df._append({'path': input_image, 'age': 'age7', 'gender': 3, 'face': 1}, ignore_index=True)
 
     df.to_csv('Data/noFace_classification.csv')
-
 
 def createFaceCSV_regression():
     # get the path/directory
@@ -213,7 +162,3 @@ def createNoFaceCSV_regression():
         df = df._append({'path': input_image, 'age': '0', 'gender': 3, 'face': 1}, ignore_index=True)
 
     df.to_csv('Data/noFace_regression.csv')
-
-
-createFaceCSV_regression()
-createNoFaceCSV_regression()
