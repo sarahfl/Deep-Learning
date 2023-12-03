e978d23b070cb5e3d63d82a60fb63e31fe4bd0bd
sarah.flohr <sarah.flohr@uni-bielefeld.de>
2023-12-03 10:57:27 +0100

    testing classification model

--- MS3/analysis/plot.py
+++ MS3/analysis/plot.py
@@ -2,12 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-modelType = 'model1_classification'
+
 def plotHistory(path_to_model):
     # Lade die Daten aus den CSV-Dateien
     age_history = pd.read_csv(os.path.join(path_to_model, 'history_age.csv'))
-    gender_history = pd.read_csv(os.path.join(path_to_model,'history_gender.csv'))
-    face_history = pd.read_csv(os.path.join(path_to_model,'history_face.csv'))
+    gender_history = pd.read_csv(os.path.join(path_to_model, 'history_gender.csv'))
+    face_history = pd.read_csv(os.path.join(path_to_model, 'history_face.csv'))
 
     ## Plot Accuracy
     plt.figure(figsize=(12, 6))
@@ -56,5 +56,125 @@ def plotHistory(path_to_model):
     plt.savefig('/home/sarah/Deep-Learning/MS3/Model/{}/loss.png'.format(modelType))
     plt.show()
 
+
+def plotBoxplot():
+    path_to_model_1 = '/home/sarah/Deep-Learning/MS3/Model/model1_classification'
+    path_to_model_2 = '/home/sarah/Deep-Learning/MS3/Model/model2_classification'
+    path_to_model_3 = '/home/sarah/Deep-Learning/MS3/Model/model3_classification'
+    path_to_model_4 = '/home/sarah/Deep-Learning/MS3/Model/model4_classification'
+    path_to_model_5 = '/home/sarah/Deep-Learning/MS3/Model/model5_classification'
+    path_to_model_6 = '/home/sarah/Deep-Learning/MS3/Model/model6_classification'
+    path_to_model_7 = '/home/sarah/Deep-Learning/MS3/Model/model7_classification'
+
+    age_history_1 = pd.read_csv(os.path.join(path_to_model_1, 'history_age.csv'))
+    gender_history_1 = pd.read_csv(os.path.join(path_to_model_1, 'history_gender.csv'))
+    face_history_1 = pd.read_csv(os.path.join(path_to_model_1, 'history_face.csv'))
+
+    age_history_2 = pd.read_csv(os.path.join(path_to_model_2, 'history_age.csv'))
+    gender_history_2 = pd.read_csv(os.path.join(path_to_model_2, 'history_gender.csv'))
+    face_history_2 = pd.read_csv(os.path.join(path_to_model_2, 'history_face.csv'))
+
+    age_history_3 = pd.read_csv(os.path.join(path_to_model_3, 'history_age.csv'))
+    gender_history_3 = pd.read_csv(os.path.join(path_to_model_3, 'history_gender.csv'))
+    face_history_3 = pd.read_csv(os.path.join(path_to_model_3, 'history_face.csv'))
+
+    age_history_4 = pd.read_csv(os.path.join(path_to_model_4, 'history_age.csv'))
+    gender_history_4 = pd.read_csv(os.path.join(path_to_model_4, 'history_gender.csv'))
+    face_history_4 = pd.read_csv(os.path.join(path_to_model_4, 'history_face.csv'))
+
+    age_history_5 = pd.read_csv(os.path.join(path_to_model_5, 'history_age.csv'))
+    gender_history_5 = pd.read_csv(os.path.join(path_to_model_5, 'history_gender.csv'))
+    face_history_5 = pd.read_csv(os.path.join(path_to_model_5, 'history_face.csv'))
+
+    age_history_6 = pd.read_csv(os.path.join(path_to_model_6, 'history_age.csv'))
+    gender_history_6 = pd.read_csv(os.path.join(path_to_model_6, 'history_gender.csv'))
+    face_history_6 = pd.read_csv(os.path.join(path_to_model_6, 'history_face.csv'))
+
+    age_history_7 = pd.read_csv(os.path.join(path_to_model_7, 'history_age.csv'))
+    gender_history_7 = pd.read_csv(os.path.join(path_to_model_7, 'history_gender.csv'))
+    face_history_7 = pd.read_csv(os.path.join(path_to_model_7, 'history_face.csv'))
+
+    age_accuracy_1 = age_history_1['accuracy'].to_numpy().max()
+    age_accuracy_2 = age_history_2['accuracy'].to_numpy().max()
+    age_accuracy_3 = age_history_3['accuracy'].to_numpy().max()
+    age_accuracy_4 = age_history_4['accuracy'].to_numpy().max()
+    age_accuracy_5 = age_history_5['accuracy'].to_numpy().max()
+    age_accuracy_6 = age_history_6['accuracy'].to_numpy().max()
+    age_accuracy_7 = age_history_7['accuracy'].to_numpy().max()
+
+    gender_accuracy_1 = gender_history_1['accuracy'].to_numpy().max()
+    gender_accuracy_2 = gender_history_2['accuracy'].to_numpy().max()
+    gender_accuracy_3 = gender_history_3['accuracy'].to_numpy().max()
+    gender_accuracy_4 = gender_history_4['accuracy'].to_numpy().max()
+    gender_accuracy_5 = gender_history_5['accuracy'].to_numpy().max()
+    gender_accuracy_6 = gender_history_6['accuracy'].to_numpy().max()
+    gender_accuracy_7 = gender_history_7['accuracy'].to_numpy().max()
+
+    face_accuracy_1 = face_history_1['accuracy'].to_numpy().max()
+    face_accuracy_2 = face_history_2['accuracy'].to_numpy().max()
+    face_accuracy_3 = face_history_3['accuracy'].to_numpy().max()
+    face_accuracy_4 = face_history_4['accuracy'].to_numpy().max()
+    face_accuracy_5 = face_history_5['accuracy'].to_numpy().max()
+    face_accuracy_6 = face_history_6['accuracy'].to_numpy().max()
+    face_accuracy_7 = face_history_7['accuracy'].to_numpy().max()
+
+    age_loss_1 = age_history_1['loss'].to_numpy().min()
+    age_loss_2 = age_history_2['loss'].to_numpy().min()
+    age_loss_3 = age_history_3['loss'].to_numpy().min()
+    age_loss_4 = age_history_4['loss'].to_numpy().min()
+    age_loss_5 = age_history_5['loss'].to_numpy().min()
+    age_loss_6 = age_history_6['loss'].to_numpy().min()
+    age_loss_7 = age_history_7['loss'].to_numpy().min()
+
+    gender_loss_1 = gender_history_1['loss'].to_numpy().min()
+    gender_loss_2 = gender_history_2['loss'].to_numpy().min()
+    gender_loss_3 = gender_history_3['loss'].to_numpy().min()
+    gender_loss_4 = gender_history_4['loss'].to_numpy().min()
+    gender_loss_5 = gender_history_5['loss'].to_numpy().min()
+    gender_loss_6 = gender_history_6['loss'].to_numpy().min()
+    gender_loss_7 = gender_history_7['loss'].to_numpy().min()
+
+    face_loss_1 = face_history_1['loss'].to_numpy().min()
+    face_loss_2 = face_history_2['loss'].to_numpy().min()
+    face_loss_3 = face_history_3['loss'].to_numpy().min()
+    face_loss_4 = face_history_4['loss'].to_numpy().min()
+    face_loss_5 = face_history_5['loss'].to_numpy().min()
+    face_loss_6 = face_history_6['loss'].to_numpy().min()
+    face_loss_7 = face_history_7['loss'].to_numpy().min()
+
+    data_accuracy = [[age_accuracy_1, age_accuracy_2, age_accuracy_3, age_accuracy_4, age_accuracy_5, age_accuracy_6,
+                      age_accuracy_7],
+                     [gender_accuracy_1, gender_accuracy_2, gender_accuracy_3, gender_accuracy_4, gender_accuracy_5,
+                      gender_accuracy_6, gender_accuracy_7],
+                     [face_accuracy_1, face_accuracy_2, face_accuracy_3, face_accuracy_4, face_accuracy_5,
+                      face_accuracy_6, face_accuracy_7]]
+
+    # Boxplot
+    plt.boxplot(data_accuracy, labels=['Age', 'Gender', 'Face'])
+    # plt.xlabel('Parameter Sets')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy für Age, Gender und Face')
+    plt.savefig('boxplot_accuracy.png')
+    plt.show()
+
+    data_loss = [[age_loss_1, age_loss_2, age_loss_3, age_loss_4, age_loss_5, age_loss_6, age_loss_7],
+                 [gender_loss_1, gender_loss_2, gender_loss_3, gender_loss_4, gender_loss_5, gender_loss_6,
+                  gender_loss_7],
+                 [face_loss_1, face_loss_2, face_loss_3, face_loss_4, face_loss_5, face_loss_6, face_loss_7]
+                 ]
+
+    print(data_loss)
+
+    plt.boxplot(data_loss, labels=['Age', 'Gender', 'Face'])
+    # plt.xlabel('Parameter Sets')
+    plt.ylabel('Loss')
+    plt.title('Loss für Age, Gender und Face')
+    plt.savefig('boxplot_loss.png')
+    plt.show()
+
+
+modelType = 'model10_classification'
 path_to_model = '/home/sarah/Deep-Learning/MS3/Model/{}'.format(modelType)
 plotHistory(path_to_model)
+
+# plotBoxplot()
