2bafb82ef5992e6b99336d83432fd5e604e8b455
XaverStiensmeier <xaverstiensmeier@gmx.de>
2024-01-13 11:58:40 +0100

    changed basic structure unchecked

--- MS4/siamese.py
+++ MS4/siamese.py
@@ -1,24 +1,44 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import GlobalAveragePooling2D
-from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import RandomFlip
+from tensorflow.keras.layers import RandomRotation
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras import Sequential
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 
 def build_siamese_model(input_shape, embedding_dim=48):
     # specify the inputs for the feature extractor network
     inputs = Input(input_shape)
 
+    base_model = MobileNetV2(
+        weights='imagenet',  # Load weights pre-trained on ImageNet.
+        input_shape=input_shape,
+        include_top=False,
+    )
+    data_augmentation = Sequential([
+        RandomFlip('horizontal'),
+        RandomRotation(0.2),
+    ])
+
     # first
-    x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Dropout(0.3)(x)
+    x = data_augmentation(inputs)
+    x = preprocess_input(x)
+    x = base_model(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1280, activation='relu')(x)
+    x = Dropout(0.5)(x)
+
     # second
-    x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-    x = MaxPooling2D(pool_size=2)(x)
-    x = Dropout(0.3)(x)
+    x = data_augmentation(x)
+    x = preprocess_input(x)
+    x = base_model(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1280, activation='relu')(x)
+    x = Dropout(0.5)(x)
 
     pooled_output = GlobalAveragePooling2D()(x)
     outputs = Dense(embedding_dim)(pooled_output)
