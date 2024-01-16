1ad94bf986bf54657db5e24146f8349b3037a947
XaverStiensmeier <xaverstiensmeier@gmx.de>
2024-01-16 18:11:19 +0100

    resolved issues with mobile net and tried to use tensorflow earlier

--- MS4/siamese.py
+++ MS4/siamese.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import RandomFlip
-from tensorflow.keras.layers import RandomRotation
+from tensorflow.keras.layers import RandomRotation, Conv2D, MaxPooling2D
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras import Sequential
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
@@ -13,10 +13,10 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 def build_siamese_model(input_shape, embedding_dim=48):
     # specify the inputs for the feature extractor network
     print(input_shape)
-    inputs = Input(input_shape)
+    inputs = Input(shape=input_shape)
     base_model = MobileNetV2(
         weights='imagenet',  # Load weights pre-trained on ImageNet.
-        input_shape=(input_shape[0], input_shape[1], 3),  # Ensure input has three channels (RGB)
+        input_shape=input_shape,  # Ensure input has three channels (RGB)
         include_top=False,
     )
     data_augmentation = Sequential([
@@ -24,19 +24,39 @@ def build_siamese_model(input_shape, embedding_dim=48):
         RandomRotation(0.2),
     ])
 
+    inputs = Input((200, 200, 3))
+    x = Conv2D(96, (11, 11), padding="same", activation="relu")(inputs)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.3)(x)
+
+    x = Conv2D(256, (5, 5), padding="same", activation="relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.3)(x)
+
+    x = Conv2D(384, (3, 3), padding="same", activation="relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.3)(x)
+
+    pooled_output = GlobalAveragePooling2D()(x)
+    pooled_output = Dense(1024)(pooled_output)
+    outputs = Dense(128)(pooled_output)
+
+    model = Model(inputs, outputs)
+    return model
+
     # first
-    x = data_augmentation(inputs)
-    x = preprocess_input(x)
-    x = base_model(x)
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(1280, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    # x = data_augmentation(inputs)
+    # x = preprocess_input(x)
+    # x = base_model(x)
+    # x = GlobalAveragePooling2D()(x)
+    # x = Dense(1280, activation='relu')(x)
+    # x = Dropout(0.5)(x)
 
     # second
-    x = base_model(x)
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(1280, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    # x = base_model(x)
+    # x = GlobalAveragePooling2D()(x)
+    # x = Dense(1280, activation='relu')(x)
+    # x = Dropout(0.5)(x)
     #
     # # define the first set of CONV => RELU => POOL => DROPOUT layers
     # x = Conv2D(64, (3, 3), padding="same", activation="relu")(inputs)  # Adjust filter size to 3x3
@@ -48,8 +68,8 @@ def build_siamese_model(input_shape, embedding_dim=48):
     # x = MaxPooling2D(pool_size=(2, 2))(x)
     # x = Dropout(0.3)(x)
 
-    pooled_output = GlobalAveragePooling2D()(x)
-    outputs = Dense(embedding_dim)(pooled_output)
+    # pooled_output = GlobalAveragePooling2D()(x)
+    outputs = Dense(embedding_dim)(x)
     model = Model(inputs, outputs)
 
     return model
--- MS4/train_siamese.py
+++ MS4/train_siamese.py
@@ -13,6 +13,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.data import AUTOTUNE
 from tensorflow.data import Dataset
 from tensorflow.keras.datasets import mnist
+from tensorflow.data import Dataset
 import numpy as np
 import logging
 import os
@@ -50,29 +51,60 @@ def do_promis():
     logging.info("[INFO] loading promis dataset...")
     df = utils.read_csv()
     df["image"] = df["path"].apply(utils.load_and_preprocess_image)
+    # Check encoding
+    # print(df['name'][0])
+    code_to_category = dict(enumerate(df['name'].astype('category').cat.categories))
     df['name'] = df['name'].astype('category').cat.codes
+    # print(df['name'][0])
+    # print(df['name'].map(code_to_category)[0])
 
     # pair is a list of image pairs
     # label is whether they are showing the same person or not
     pair, label = utils.make_pairs(df['image'], df['name'])
-
-    dataset_size = len(label)
-    train_size = int(0.7 * dataset_size)
-    val_size = int(0.15 * dataset_size)
+    print("Before slicing")
+    dataset = Dataset.from_tensor_slices(([pair[:, 0], pair[:, 1]], label))
+    print("After slicing")
+    print('Aufbau des Datensets: ', dataset.element_spec)
+    dataset_size = len(df)
+    ##
+    # -- SPLIT DATASET INTO TRAIN, VAL AND TEST --
+    # train=0.8, validation=0.1, test=0.1
+    train_size = int(0.8 * dataset_size)
+    val_size = int(0.1 * dataset_size)
     test_size = dataset_size - train_size - val_size
 
-    pair_train = pair[:train_size]
-    pair_val = pair[train_size:train_size+val_size]
-    pair_test = pair[train_size + val_size:]
+    train_dataset = dataset.take(train_size)
+    val_dataset = dataset.skip(train_size).take(val_size)
+    test_dataset = dataset.skip(train_size + val_size)
 
-    label_train = label[:train_size]
-    label_val = label[train_size:train_size+val_size]
-    label_test = label[train_size + val_size:]
+    train_dataset = train_dataset.batch(configuration.BATCH_SIZE)
+    val_dataset = val_dataset.batch(configuration.BATCH_SIZE)
+    test_dataset = test_dataset.batch(configuration.BATCH_SIZE)
 
-    return (pair_train, label_train), (pair_val, label_val), (pair_test, label_test)
+    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+    val_dataset = val_dataset.prefetch(buffer_size=AUTOTUNE)
+    test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+    # dataset_size = len(label)
+    # train_size = int(0.7 * dataset_size)
+    # val_size = int(0.15 * dataset_size)
+    # test_size = dataset_size - train_size - val_size
+    #
+    # pair_train = pair[:train_size]
+    # pair_val = pair[train_size:train_size+val_size]
+    # pair_test = pair[train_size + val_size:]
+    #
+    # label_train = label[:train_size]
+    # label_val = label[train_size:train_size+val_size]
+    # label_test = label[train_size + val_size:]
+
+    # return (pair_train, label_train), (pair_val, label_val), (pair_test, label_test)
+    print("Created datasets")
+    return train_dataset, val_dataset, test_dataset
 
 
-(pair_train, label_train), (pair_val, label_val), (pair_test, val_test) = do_promis()
+# (pair_train, label_train), (pair_val, label_val), (pair_test, val_test) = do_promis()
+train_dataset, val_dataset, test_dataset = do_promis()
 
 # configure siamese
 logging.info("Building siamese network...")
@@ -102,8 +134,10 @@ early_stopping = EarlyStopping(
 # train the model
 logging.info("Training model...")
 history = model.fit(
-    [pair_train[:, 0], pair_train[:, 1]], label_train[:],
-    validation_data=([pair_val[:, 0], pair_val[:, 1]], label_val[:]),
+    # [pair_train[:, 0], pair_train[:, 1]], label_train[:],
+    # validation_data=([pair_val[:, 0], pair_val[:, 1]], label_val[:]),
+    train_dataset,
+    val_dataset,
     batch_size=configuration.BATCH_SIZE,
     epochs=configuration.EPOCHS,
     callbacks=[early_stopping])
@@ -111,4 +145,3 @@ history = model.fit(
 model.save(configuration.MODEL_PATH)
 logging.info("Plotting training history...")
 utils.plot_training(history.history, configuration.PLOT_PATH)
-
